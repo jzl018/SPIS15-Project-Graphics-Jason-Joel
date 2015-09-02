2b3705574fc51e0af39144804e8570a1270b0d9a
Joel Loo <jcloo@ucsd.edu>
2015-09-02 15:12:12 -0700

    Finished

--- Posterize.py
+++ Posterize.py
@@ -1,10 +1,14 @@
-from PIL import Image, ImageDraw
+## Needs PIL, pygame, VideoCapture, time to run!!! Make sure all are installed"
 
-mohan = Image.open("Pictures/mohan.jpg")
-bear = Image.open("Desktop/stoneteddybear.jpg")
-butterfly = Image.open("Pictures/blue.jpg")
-photo = Image.open("Pictures/photo.png")
-aaron = Image.open("Pictures/aaron.jpg")
+from PIL import *
+from pygame import *
+from VideoCapture import *
+import time
+
+##bear = Image.open("Desktop/stoneteddybear.jpg")
+##butterfly = Image.open("Pictures/blue.jpg")
+##photo = Image.open("Pictures/photo.png")
+##aaron = Image.open("Pictures/aaron.jpg")
         
 def binarize(im, thresh):
     for x in range(im.size[0]):
@@ -27,30 +31,60 @@ def posterize(im):
             newBlue = blue & 0xC0
             poster.putpixel((x,y),(newRed,newGreen,newBlue))
     poster.show()
+    poster.save("PosterTest2.jpg")
 
-##def posterize(im, levels):
-##
-##    #Figure out thresholds for the pixel colors
-##    increment = 255//levels
-##    thresholds = range(1, 256, increment)
-##
-####    im = copy.copy(im)
-####    img = im.load()
-##    poster=Image.new("RGB", (im.size[0],im.size[1]))
-##    draw = ImageDraw.Draw(poster)
-##    #Iterate through the image
-##    for y in range(im.size[1]):
-##        for x in range(im.size[0]):
-##
-##            #Get a new RGB tuple based on thresholds
-##            new = []
-##            for c in range(3):
-##                color = im[x, y][c]
-##                level = bisect_left(thresholds, color)-1
-##                new.append(thresholds[level])
-##
-##            #Put the new pixel on the image
-##            poster[x, y] = tuple(new)
-##
-##    #Return the image
-##    poster.show()
+#def posterizePoint(im):
+
+def takeIm():
+    cam = Device()
+    cam.saveSnapshot('image.jpg')
+
+def imageCapture():
+    print "Taking initial picture in 1 second"
+    time.sleep(1)
+    cam = Device()
+    cam.saveSnapshot("initial.jpg")
+    initial = Image.open("initial.jpg")
+    (width, height) = initial.size
+    print "Taking second picture in 10..."
+    time.sleep(1)
+    print "9..."
+    time.sleep(1)
+    print "8..."
+    time.sleep(1)
+    print "7..."
+    time.sleep(1)
+    print "6..."
+    time.sleep(1)
+    print "5..."
+    time.sleep(1)
+    print "4..."
+    time.sleep(1)
+    print "3..."
+    time.sleep(1)
+    print "2..."
+    time.sleep(1)
+    print "1..."
+    time.sleep(1)
+    print "Taking photo!"
+    cam.saveSnapshot("new.jpg")
+    new = Image.open("new.jpg")
+    counter = 0
+    for x in range(width):
+        for y in range(height):
+            (initialRed,initialGreen,initialBlue) = initial.getpixel((x,y))
+            (newRed,newGreen,newBlue) = new.getpixel((x,y))
+            if newRed <= initialRed-25 or newRed >= initialRed+25:
+                if newGreen <= initialGreen-25 or newGreen >= initialGreen+25:
+                    if newBlue <= initialBlue-25 or newBlue >= initialBlue+25:
+                        counter = counter + 1
+            else:
+                pass
+    if counter >= 300000:
+        posterize(new)
+        
+    
+##def initalFrame():
+## 
+##    
+
